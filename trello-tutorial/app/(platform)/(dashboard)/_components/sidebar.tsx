1641d63de2eada564641c96f8baf1bb8ada2d22f
neko-no <101441574+neko-no@users.noreply.github.com>
2024-02-27 18:25:37 +0900

    #11 add: サイドバーの状態を管理する関数を定義

--- trello-tutorial/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ trello-tutorial/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,5 +1,48 @@
 "use client";
 
-export const Sidebar = () => {
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+
+interface SidebarProps {
+  storageKey?: string;
+}
+
+export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+  // アコーディンオンが開いているのかを管理する
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+
+  const { userMemberships, isLoaded: isLoadedList } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({ ...curr, [id]: !expanded[id] }));
+  };
   return <div>Sidebar!</div>;
 };
