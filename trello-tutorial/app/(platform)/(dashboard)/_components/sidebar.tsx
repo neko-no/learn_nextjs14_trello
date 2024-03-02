4e38affa05d2a1120d82618c3f095c3e03394a27
neko-no <101441574+neko-no@users.noreply.github.com>
2024-03-02 18:33:33 +0900

    #11 add: サイドバーとそのスケルトンの作成

--- trello-tutorial/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ trello-tutorial/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -10,12 +10,13 @@ import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Accordion } from "@/components/ui/accordion";
 
+import { NavItem, Organization } from "./nav-item";
+
 interface SidebarProps {
   storageKey?: string;
 }
 
 export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
-  // アコーディンオンが開いているのかを管理する
   const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
     storageKey,
     {}
@@ -23,8 +24,7 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
 
   const { organization: activeOrganization, isLoaded: isLoadedOrg } =
     useOrganization();
-
-  const { userMemberships, isLoaded: isLoadedList } = useOrganizationList({
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
     userMemberships: {
       infinite: true,
     },
@@ -41,21 +41,33 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
     []
   );
 
-  if (!isLoadedOrg || !isLoadedList || userMemberships.isLoading) {
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
   }
 
-  const onExpand = (id: string) => {
-    setExpanded((curr) => ({ ...curr, [id]: !expanded[id] }));
-  };
   return (
     <>
       <div className="font-medium text-xs flex items-center mb-1">
-        <span className="pl-4 ">Workspaces</span>
+        <span className="pl-4">Workspaces</span>
         <Button
           asChild
           type="button"
@@ -68,6 +80,21 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
           </Link>
         </Button>
       </div>
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
+        ))}
+      </Accordion>
     </>
   );
 };
