f43393a12846bf0b56696be0ad67206b44fb9bf5
neko-no <101441574+neko-no@users.noreply.github.com>
2024-03-09 14:57:37 +0900

    #16 add: Server Actionによるデータ作成

--- trello-tutorial/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ trello-tutorial/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,34 @@
+import { db } from "@/lib/db";
+
 const OrganizationIdPage = () => {
-  return <div>Organization Page</div>;
+  async function create(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+
+    console.log("I'm server action");
+  }
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border border-black p-1"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
